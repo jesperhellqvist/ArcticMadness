956825059de5eccf57d57f732611459f9e18ab38
mathiaselvforsen <me224qh@student.lnu.se>
2024-04-04 15:38:41 +0200

    new bg

--- src/system/Main.js
+++ src/system/Main.js
@@ -30,7 +30,10 @@ ArcticMadness.system.Main = function() {
         useGamepads:true,
         useKeyboard:true,
         framerate: 30,
-        debug: true
+        debug: true,
+        screenResolutionX: 384,
+        screenResolutionY: 216
+        
     });
 };
 
