96c3e73d1169610d7bc6fe967dfebf61f671a1bd
mathiaselvforsen <me224qh@student.lnu.se>
2024-04-10 15:20:55 +0200

    animation update

--- src/scene/game/Game.js
+++ src/scene/game/Game.js
@@ -44,7 +44,7 @@ ArcticMadness.scene.Game.prototype.constructor = ArcticMadness.scene.Game;
  */
 ArcticMadness.scene.Game.prototype.init = function() {
     rune.scene.Scene.prototype.init.call(this);
-    var bg = new rune.display.Graphic(0,0,960,540,"bg2");
+    var bg = new rune.display.Graphic(0,0,1280,720,"bg_720");
 
   
     
@@ -53,8 +53,8 @@ ArcticMadness.scene.Game.prototype.init = function() {
     text.autoSize = true;
     text.center = this.application.screen.center;
 
-    var player = new ArcticMadness.entity.Player(100, 100, "penguin64", {left: "A", right: "D", up: "W", down: "S", shoot: "SPACE"});
-     var player2 = new ArcticMadness.entity.Player(200, 200, "penguin64", {left: "LEFT", right: "RIGHT", up: "UP", down: "DOWN", shoot: "SPACE"});
+    var player = new ArcticMadness.entity.Player(100, 100, "penguin_texture_64", {left: "A", right: "D", up: "W", down: "S", shoot: "SPACE"});
+     var player2 = new ArcticMadness.entity.Player(200, 200, "penguin_texture_64", {left: "LEFT", right: "RIGHT", up: "UP", down: "DOWN", shoot: "SPACE"});
 
   
   
