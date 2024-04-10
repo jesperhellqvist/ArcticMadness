d188e9aba06ce210add5507785ad83f99bb253a4
jesper hellqvist <jh225ci@student.lnu.se>
2024-04-10 16:49:34 +0200

    Player: movment with gamepad ok

--- src/entity/Player.js
+++ src/entity/Player.js
@@ -2,21 +2,16 @@
 // Constructor scope
 //--------------------------------
 
-ArcticMadness.entity.Player = function (x, y, penguin, controls) {
+ArcticMadness.entity.Player = function (x, y, penguin, controls, gamepad) {
   this.health = 100; // Player health
   this.controls = controls; // Player controls on keyboard
   this.x = x; // Player x position
   this.y = y; // Player y position
   this.angle = 0; // Player angle
-  this.gamepad = this.gamepads.get(0);
-  
-  
- 
-  
-  
-  console.log(this.gamepad);
+  this.gamepad = gamepad; // Player gamepad
 
   
+
   //this.topSpeed = 3; // Player top speed
 
   //--------------------------------------------------------------------------
@@ -46,22 +41,19 @@ ArcticMadness.entity.Player.prototype.init = function () {
   rune.display.Sprite.prototype.init.call(this);
   console.log("Player initialized");
 
-  this.animation.create("idle", [0, 1, 2,3,4], 8, true);
-  this.animation.create("walk", [5,6,7,8], 10, true);
-  this.animation.create("down", [10,11,12,13,14], 10, true);
-  this.animation.create("up", [15,16,17], 10, true);
-  
+  this.animation.create("idle", [0, 1, 2, 3, 4], 8, true);
+  this.animation.create("walk", [5, 6, 7, 8], 10, true);
+  this.animation.create("down", [10, 11, 12, 13, 14], 10, true);
+  this.animation.create("up", [15, 16, 17], 10, true);
+
   this.m_setPhysics();
 };
 
 ArcticMadness.entity.Player.prototype.update = function (step) {
   rune.display.Sprite.prototype.update.call(this, step);
-
-  if(this.gamepad.stickLeftLeft){
-    this.x--;
-  }
-
-  this.m_handleInput(this.controls);
+//this.m_handleInput(this.controls);
+  this.m_handleInputGamepad();
+  
   this.m_handleHitBox();
 };
 
@@ -92,7 +84,7 @@ ArcticMadness.entity.Player.prototype.m_handleInput = function (controls) {
       this.x += 5;
       this.velocity.x += 0.15;
       this.flippedX = false;
-        this.angle = 90;
+      this.angle = 90;
       this.animation.gotoAndPlay("walk");
     }
   }
@@ -112,7 +104,7 @@ ArcticMadness.entity.Player.prototype.m_handleInput = function (controls) {
     } else {
       this.y += 5;
       this.velocity.y += 0.15;
-        this.angle = 180;
+      this.angle = 180;
       this.animation.gotoAndPlay("down");
     }
   }
@@ -120,7 +112,7 @@ ArcticMadness.entity.Player.prototype.m_handleInput = function (controls) {
   if (this.keyboard.justPressed(controls.shoot)) {
     this.m_handleShoot(this.angle);
   }
-  
+
   if (
     !this.keyboard.pressed(controls.left) &&
     !this.keyboard.pressed(controls.right) &&
@@ -130,17 +122,67 @@ ArcticMadness.entity.Player.prototype.m_handleInput = function (controls) {
     this.flippedX = false;
     this.animation.gotoAndPlay("idle");
   }
+};
 
+ArcticMadness.entity.Player.prototype.m_handleInputGamepad = function () {
+ if(this.gamepad.justPressed(0)){
+  this.m_handleShoot(this.angle);
+ }
 
+ if(this.gamepad.stickLeftLeft){
+  if (this.x <= 0) {
+    this.x = 0;
+  } else {
+    this.x -= 5;
+    this.velocity.x -= 0.15;
+    this.flippedX = true;
+    this.angle = 270;
+    this.animation.gotoAndPlay("walk");
+  }
+ }
+
+  if(this.gamepad.stickLeftRight){
+    if (this.x >= 1250) {
+      this.x = 1250;
+    } else {
+      this.x += 5;
+      this.velocity.x += 0.15;
+      this.flippedX = false;
+      this.angle = 90;
+      this.animation.gotoAndPlay("walk");
+    }
+  }
+
+  if(this.gamepad.stickLeftUp){
+    if (this.y <= 0) {
+      this.y = 0;
+    } else {
+      this.y -= 5;
+      this.velocity.y -= 0.15;
+      this.angle = 0;
+      this.animation.gotoAndPlay("up");
+    }
+  }
+
+  if(this.gamepad.stickLeftDown){
+    if (this.y >= 700) {
+      this.y = 700;
+    } else {
+      this.y += 5;
+      this.velocity.y += 0.15;
+      this.angle = 180;
+      this.animation.gotoAndPlay("down");
+    }
+  }
   
 };
 
 ArcticMadness.entity.Player.prototype.m_handleShoot = function (angle) {
-    console.log(angle);
-    this.bullet = new ArcticMadness.entity.Bullet(this.x, this.y, angle);
-    this.bullet.y = this.y + 30;
-    this.stage.addChild(this.bullet);
-    };
+  console.log(angle);
+  this.bullet = new ArcticMadness.entity.Bullet(this.x, this.y, angle);
+  this.bullet.y = this.y + 30;
+  this.stage.addChild(this.bullet);
+};
 
 ArcticMadness.entity.Player.prototype.m_setPhysics = function () {
   this.velocity.drag.x = 0.02;
@@ -149,11 +191,8 @@ ArcticMadness.entity.Player.prototype.m_setPhysics = function () {
   this.velocity.max.x = 2.5;
 };
 
-
-
 ArcticMadness.entity.Player.prototype.m_handleHitBox = function () {
   this.hitbox.set();
   this.debug = true;
   this.debugColor = "#FF0000";
-  
 };
--- src/scene/game/Game.js
+++ src/scene/game/Game.js
@@ -10,19 +10,18 @@
  *
  * @class
  * @classdesc
- * 
+ *
  * Game scene.
  */
-ArcticMadness.scene.Game = function() {
+ArcticMadness.scene.Game = function () {
+  //--------------------------------------------------------------------------
+  // Super call
+  //--------------------------------------------------------------------------
 
-    //--------------------------------------------------------------------------
-    // Super call
-    //--------------------------------------------------------------------------
-    
-    /**
-     * Calls the constructor method of the super class.
-     */
-    rune.scene.Scene.call(this);
+  /**
+   * Calls the constructor method of the super class.
+   */
+  rune.scene.Scene.call(this);
 };
 
 //------------------------------------------------------------------------------
@@ -37,59 +36,55 @@ ArcticMadness.scene.Game.prototype.constructor = ArcticMadness.scene.Game;
 //------------------------------------------------------------------------------
 
 /**
- * This method is automatically executed once after the scene is instantiated. 
+ * This method is automatically executed once after the scene is instantiated.
  * The method is used to create objects to be used within the scene.
  *
  * @returns {undefined}
  */
-ArcticMadness.scene.Game.prototype.init = function() {
-    rune.scene.Scene.prototype.init.call(this);
-    var bg = new rune.display.Graphic(0,0,1280,720,"bg_720");
+ArcticMadness.scene.Game.prototype.init = function () {
+  rune.scene.Scene.prototype.init.call(this);
+  var bg = new rune.display.Graphic(0, 0, 1280, 720, "bg_720");
 
-  
-    
-    
-    var text = new rune.text.BitmapField("Arctic Madness!!");
-    text.autoSize = true;
-    text.center = this.application.screen.center;
+  var text = new rune.text.BitmapField("Arctic Madness!!");
+  text.autoSize = true;
+  text.center = this.application.screen.center;
 
-    var player = new ArcticMadness.entity.Player(100, 100, "penguin_texture_64", {left: "A", right: "D", up: "W", down: "S", shoot: "SPACE"});
-     var player2 = new ArcticMadness.entity.Player(200, 200, "penguin_texture_64", {left: "LEFT", right: "RIGHT", up: "UP", down: "DOWN", shoot: "SPACE"});
+  var player = new ArcticMadness.entity.Player(100, 100, "penguin_texture_64", {
+    left: "A",
+    right: "D",
+    up: "W",
+    down: "S",
+    shoot: "SPACE",
+  }, this.gamepads.get(0));
+  //  var player2 = new ArcticMadness.entity.Player(200, 200, "penguin_texture_64", {left: "LEFT", right: "RIGHT", up: "UP", down: "DOWN", shoot: "SPACE"});
 
-  
-  
+  this.stage.addChild(bg);
+  this.stage.addChild(player);
+  //this.stage.addChild(player2);
 
-   
-
-
-    this.stage.addChild(bg);
-    this.stage.addChild(player);
-    this.stage.addChild(player2);
-  
-    this.stage.addChild(text);
+  this.stage.addChild(text);
 };
 
 /**
- * This method is automatically executed once per "tick". The method is used for 
+ * This method is automatically executed once per "tick". The method is used for
  * calculations such as application logic.
  *
  * @param {number} step Fixed time step.
  *
  * @returns {undefined}
  */
-ArcticMadness.scene.Game.prototype.update = function(step) {
-    rune.scene.Scene.prototype.update.call(this, step);
+ArcticMadness.scene.Game.prototype.update = function (step) {
+  rune.scene.Scene.prototype.update.call(this, step);
 };
 
 /**
- * This method is automatically called once just before the scene ends. Use 
- * the method to reset references and remove objects that no longer need to 
- * exist when the scene is destroyed. The process is performed in order to 
+ * This method is automatically called once just before the scene ends. Use
+ * the method to reset references and remove objects that no longer need to
+ * exist when the scene is destroyed. The process is performed in order to
  * avoid memory leaks.
  *
  * @returns {undefined}
  */
-ArcticMadness.scene.Game.prototype.dispose = function() {
-    rune.scene.Scene.prototype.dispose.call(this);
-
+ArcticMadness.scene.Game.prototype.dispose = function () {
+  rune.scene.Scene.prototype.dispose.call(this);
 };
