1c3bd162343e48d502e926f526a210233b22e55f
jesper hellqvist <jh225ci@student.lnu.se>
2024-04-25 09:11:21 +0200

    Build: first build complete

--- scripts/shell/build.sh
+++ scripts/shell/build.sh
@@ -9,6 +9,12 @@ npx google-closure-compiler \
 --js "./../../lib/rune.js" \
 --js "./../../src/scope/Manifest.js" \
 --js "./../../src/data/resource/Requests.js" \
+--js "./../../src/entity/Bullet.js" \
+--js "./../../src/entity/Gun.js" \
+--js "./../../src/entity/Enemy.js" \
+--js "./../../src/entity/Player.js" \
+--js "./../../src/entity/Players.js" \
+--js "./../../src/map/Map.js" \
 --js "./../../src/scene/game/Game.js" \
 --js "./../../src/system/Main.js" \
 --js "./../../src/scope/Alias.js" \
