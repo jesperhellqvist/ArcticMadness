5e6077b31af915361a4edd5d4bc2c2bf26c848d9
mathiaselvforsen <me224qh@student.lnu.se>
2024-05-07 10:14:27 +0200

    Menu

---
+++ src/scene/menu/Menu.js
@@ -0,0 +1,60 @@
+//------------------------------------------------------------------------------
+// Constructor scope
+//------------------------------------------------------------------------------
+
+/**
+ * Creates a new object.
+ *
+ * @constructor
+ * @extends rune.scene.Scene
+ *
+ * @class
+ * @classdesc
+ * 
+ * Game state.
+ */
+
+ArcticMadness.scene.Menu = function () {
+
+
+    rune.scene.Scene.call(this);
+
+};
+
+//------------------------------------------------------------------------------
+// Inheritance
+//------------------------------------------------------------------------------
+
+ArcticMadness.scene.Menu.prototype = Object.create(rune.scene.Scene.prototype);
+ArcticMadness.scene.Menu.prototype.constructor = ArcticMadness.scene.Menu;
+
+//------------------------------------------------------------------------------
+// Override public prototype methods (ENGINE)
+//------------------------------------------------------------------------------
+
+/**
+ * @inheritDoc
+ */
+
+ArcticMadness.scene.Menu.prototype.init = function () {
+    rune.scene.Scene.prototype.init.call(this);
+    var t = new rune.text.BitmapField("Press Space");
+        t.autoSize = true;
+        t.center = this.application.screen.center;
+        
+    this.stage.addChild(t);
+    console.log("Menu init");
+}
+
+/**
+ * @inheritDoc
+ */
+
+ArcticMadness.scene.Menu.prototype.update = function (step) {
+    rune.scene.Scene.prototype.update.call(this, step);
+    if (this.keyboard.justPressed("space")) {
+        this.application.scenes.load([
+            new ArcticMadness.scene.Game()
+        ]);
+    }
+}
